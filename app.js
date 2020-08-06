e63ce11e2e537ed6c7b7ec709d49924d6f7f976e
Matthew Hummer <biggreyson@gmail.com>
2020-08-06 13:11:11 -0600

    add dynamic production connection

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express  = require('express');
+const http     = require('http');
+const socketio = require('socket.io');
+const redis    = require('redis');
+
 const port = process.env.PORT || 8000;
-const redis = require('redis').createClient();
-if (process.env.NODE_ENV !== 'production') {
+const production = process.env.NODE_ENV === 'production';
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+if (!production) {
   require('dotenv').config();
 }
 
-redis.subscribe('wellnav:messages');
+const redisClient = production
+  ? redis.createClient({ url: process.env.REDIS_URL })
+  : redis.createClient();
+
+redisClient.subscribe('wellnav:messages');
 
 app.get('/redis-test', (req, res, next) => {
-  res.send(redis.ping());
+  res.send(redisClient.ping());
 });
 
 io.on('connection', function(socket){
-  redis.on('message', function(channel, message){
+  redisClient.on('message', function(channel, message){
     socket.emit('new-message', JSON.parse(message));
   });
 });
 
 if (process.env.NODE_ENV !== 'production') {
   server.listen(port, '0.0.0.0', () => {
-    console.log('listening on ', '0.0.0.0:'+port);
+    console.log('listening on ', '0.0.0.0:' + port);
   });
 } else{
   server.listen(port,()=>{
