d94416dd6b55a8fc048bd69f7118c01b32d29431
Matthew Hummer <biggreyson@gmail.com>
2020-08-06 18:02:17 -0600

    add room namespacing

--- app.js
+++ app.js
@@ -23,18 +23,28 @@ app.get('/redis-test', (req, res, next) => {
   res.send(redisClient.ping());
 });
 
-io.on('connection', function(socket){
+
+/*
+  TODO: I suspect that there's an issue here. I worry this is adding multiple listeners
+  every time a connection is made. If we get to the point where we can test 3+ people in
+  a single room and we see messages being added multiple times even though only a single
+  record is created, I bet this will be the cause. If that happens we'll need to move to some kind of "registry" for our rooms and their emissions.
+*/
+io.of(/^\/chat-\d*$/).on('connection', function(socket){
+  const chatRoom = socket.nsp;
+  // chatRoom.emit('hello', `${socket.id} has joined ${chatRoom.name}`);
+
   redisClient.on('message', function(channel, message){
     socket.emit('new-message', JSON.parse(message));
   });
 });
 
-if (process.env.NODE_ENV !== 'production') {
+if (!production) {
   server.listen(port, '0.0.0.0', () => {
     console.log('listening on ', '0.0.0.0:' + port);
   });
 } else{
-  server.listen(port,()=>{
+  server.listen(port, () => {
     console.log('listening on ', port);
   });
 }
