23f22d78bba797cb9a8a13a1780ff1681383615c
Matthew Hummer <biggreyson@gmail.com>
2020-08-09 10:04:27 -0600

    improve namespace usage with redis

--- app.js
+++ app.js
@@ -12,31 +12,26 @@ const io = socketio(server);
 if (!production) {
   require('dotenv').config();
 }
+const rooms = {};
 
-const redisClient = production
-  ? redis.createClient({ url: process.env.REDIS_URL })
-  : redis.createClient();
-
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
 redisClient.subscribe('wellnav:messages');
-
-app.get('/redis-test', (req, res, next) => {
-  res.send(redisClient.ping());
+redisClient.on('message', (channel, messageData) => {
+  const message = JSON.parse(messageData);
+  const namespaceName = `chat-${message.chat_id}`;
+  io.of(namespaceName).emit('new-message', message);
 });
 
-
-/*
-  TODO: I suspect that there's an issue here. I worry this is adding multiple listeners
-  every time a connection is made. If we get to the point where we can test 3+ people in
-  a single room and we see messages being added multiple times even though only a single
-  record is created, I bet this will be the cause. If that happens we'll need to move to some kind of "registry" for our rooms and their emissions.
-*/
-io.of(/^\/chat-\d*$/).on('connection', function(socket){
-  const chatRoom = socket.nsp;
-  // chatRoom.emit('hello', `${socket.id} has joined ${chatRoom.name}`);
-
-  redisClient.on('message', function(channel, message){
-    socket.emit('new-message', JSON.parse(message));
-  });
+io.of(/^\/chat-\d*$/).on('connection', socket => {
+  // const namespace = socket.nsp;
+  // const namespaceName = namespace.name; // includes leading slash
+  // rooms[namespaceName] = namespace;
+  // console.log(rooms)
+  // TODO: add custom socket event handlers here.
+  // socket.on('disconnect', function(){
+  //   if(!Object.keys(namespace.connected).length) delete rooms[namespaceName];
+  //   console.log(rooms)
+  // })
 });
 
 if (!production) {
